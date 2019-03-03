3355d8d15065189c3e9459f73e4477259a8bf11b
Brayan Baron <brayan.barona@konradlorenz.edu.co>
2019-03-03 01:47:36 -0500

    actualizacion y configuracion de ruta de perfil

--- Codigo/src/app/app-routing.module.ts
+++ Codigo/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { HomePageComponent } from '../app/home-page/home-page.component';
 import { SocialPageComponent } from '../app/social-page/social-page.component';
 import { ForgotPasswordComponent } from '../app/forgot-password/forgot-password.component';
 import { FormPersonComponent } from '../app/form-person/form-person.component';
+import { PerfilComponent } from '../app/perfil/perfil.component';
 
 const routes: Routes = [];
 //BEGIN OF CCSANCHEZC 15/02/2019 7:29
@@ -22,6 +23,7 @@ export const rootRouterConfig: Routes = [
   { path: 'social', component: SocialPageComponent, canActivate: [AuthGuard] },
   { path: 'forgot', component: ForgotPasswordComponent },
   { path: 'form-person', component: FormPersonComponent, },
+  { path: 'perfil', component: PerfilComponent, },
   { path: '**', component: NotFoundComponent },
 
 ];
