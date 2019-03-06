8ab12bdc3e17d6cf4debf8c5ab8c248573822b93
Brayan Baron <brayan.barona@konradlorenz.edu.co>
2019-03-06 00:20:29 -0500

    cambios perfil y foto perfil

--- Codigo/src/app/navbar/navbar.component.ts
+++ Codigo/src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../Core/auth.service';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -10,12 +11,17 @@ export class NavbarComponent implements OnInit {
   public nombreUsuario: string;
   public emailUsuario: string;
   public fotoUsuario: string;
+  private fotico: string;
   constructor(
     public authService: AuthService
-  ) { }
+
+  ) {
+
+    this.fotico = 'https://angellomix.com/wp-content/uploads/2016/10/login.png';
+  }
 
   ngOnInit() {
-    this.authService.getAuth().subscribe( auth => {
+    this.authService.getAuth().subscribe(auth => {
       if (auth) {
         this.isLogin = true;
         this.nombreUsuario = auth.displayName;
@@ -23,8 +29,11 @@ export class NavbarComponent implements OnInit {
         this.fotoUsuario = auth.photoURL;
       } else {
         this.isLogin = false;
+      //  this.fotoUsuario='https://angellomix.com/wp-content/uploads/2016/10/login.png';
+        
       }
-    });
+    }
+    );
   }
   onClickLogout() {
     this.authService.doLogout();
