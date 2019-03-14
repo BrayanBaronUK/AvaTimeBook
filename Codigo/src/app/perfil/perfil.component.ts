3466e1aa735123b55b1a075463eb0887353bfe76
DavidUK <36526045+c-esar@users.noreply.github.com>
2019-03-13 21:16:58 -0500

    cambios

--- Codigo/src/app/perfil/perfil.component.ts
+++ Codigo/src/app/perfil/perfil.component.ts
@@ -53,17 +53,8 @@ export class PerfilComponent implements OnInit {
     fileReader.onload = ($event: any) => {
       this.foto = $event.target.result;
 
-<<<<<<< HEAD
     }
     fileReader.readAsDataURL($event.target.files[0])
-=======
-  let fileReader = new FileReader();
-  //let ruta= fileReader.readAsDataURL($event.target.files[0])//
- //   console.log("rtua", ruta)//
-  //this.foto= ruta;
-  fileReader.onload = ($event: any) => {
-    this.foto = $event.target.result;
->>>>>>> brayan
 
 
     // let nombreFoto= $event.target.files[0].name;
@@ -77,7 +68,6 @@ export class PerfilComponent implements OnInit {
     console.log(randomId);
     const filepath = `/${randomId}`;
 
-<<<<<<< HEAD
     const fileRef = this._storage.ref(filepath);
 
     // Upload image
@@ -91,10 +81,6 @@ export class PerfilComponent implements OnInit {
       (() => this.uploadURL = fileRef.getDownloadURL())
     ).subscribe();
   }
-=======
-  //let nombreFoto= $event.target.files[0].name;//
-}
->>>>>>> brayan
 
 
 }
