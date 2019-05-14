92cb562c67de49a61c8969dd3077252de2427dea
DavidUK <36526045+c-esar@users.noreply.github.com>
2019-05-14 16:36:27 -0500

    tabla de seguir

--- Codigo/src/app/filtro-person/filtro-person.component.ts
+++ Codigo/src/app/filtro-person/filtro-person.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ServicioFiltroPersonaService } from '../Core/servicio-filtro-persona.service';
 import { UserService } from '../Core/user.service';
 import { ServicioLibroService } from '../Core/servicio-libro.service';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 @Component({
   selector: 'app-filtro-person',
   templateUrl: './filtro-person.component.html',
@@ -11,10 +12,12 @@ export class FiltroPersonComponent implements OnInit {
 
   filtrouser = [];
   public seguir = [];
-  public userFirebase :any;
+  public userFirebase: any;
   public input: any;
   public input2: any;
   public seguirBoolean = true;
+  public colum: any;
+  public guardar: any;
   public filter: any; table: any; tr: any; td: any; i: any; txtValue: any;
   public filter2: any; table2: any; tr2: any; td2: any; i2: any; txtValue2: any;
   constructor(public userservice: ServicioFiltroPersonaService,
@@ -26,52 +29,66 @@ export class FiltroPersonComponent implements OnInit {
 
   ngOnInit() {
     this.TraerPersonas();
-
   }
 
-
+  MostrarColumnas() {
+    this.colum = [
+      { field: 'nombre_libro', header: 'Nombre' },
+      { field: 'autor_libro', header: 'Apellido' },
+    ];
+  }
 
   TraerPersonas() {
-    this.userservice.getPerfiles().subscribe((usuarios) => {
-      usuarios.map((usuariosdata: any) => {
-        this.servicioLibroService.getLibros(usuariosdata.payload.doc.id).subscribe((libro) => {
+    this.userservicePerfil.CargarPersonaSeguir().subscribe((seguir) => {
+      this.userservice.getPerfiles().subscribe((usuarios) => {
 
+        // this.servicioLibroService.getLibros(usuariosdata.payload.doc.id).subscribe((libro) => {       
+        usuarios.map((usuariosdata: any) => {
           this.filtrouser.push({
             id: usuariosdata.payload.doc.id,
             data: usuariosdata.payload.doc.data(),
-            libros: [
-              libro.map(l => {
-                 // tslint:disable-next-line:no-unused-expression
-                 return  l.payload.doc.data();
-              })
-              ]
+            // libros: [
+            //   libro.map(l => {
+            //     // tslint:disable-next-line:no-unused-expression
+            //     return l.payload.doc.data();
+            //   })
+            // ]
           });
         });
-      });
-    });
-    this.userservicePerfil.CargarPersonaSeguir().subscribe((seguir) => {
-      seguir.map((s) =>{
-        this.seguir.push({
-          id: s.payload.doc.id,
-          data: s.payload.doc.data()
+        seguir.map((seguirperson: any) => {
+          for (var i = 0; i < this.filtrouser.length; i++) {
+            if (seguirperson.payload.doc.id == this.filtrouser[i].id) {
+              this.seguir.push({
+                id: seguirperson.payload.doc.id,
+                data: seguirperson.payload.doc.data(),
+                // libros: [
+                //   libro.map(l => {
+                //     return l.payload.doc.data()
+                //   })
+                // ]
+              });
+              this.filtrouser.splice(i, 1);
+            }
+          }
+
         });
       });
     });
     console.log(this.seguir);
     console.log(this.filtrouser);
   }
-  onfiltro(data,id) {
+  onfiltro(data, id) {
     this.userservicePerfil.GuardarPersonaSeguir(id, data);
     this.userservicePerfil.getPerfil().valueChanges().subscribe((user) => {
       this.userservicePerfil.GuadarPersonaSeguidor(id, user);
     });
-    
+
   }
 
   myFunctionNombre() {
     this.input = document.getElementById('myInput');
     if (this.input != null) {
-      
+
       this.filter = this.input.value.toUpperCase();
       this.table = document.getElementById('myTable');
       this.tr = this.table.getElementsByTagName('tr');
@@ -91,8 +108,4 @@ export class FiltroPersonComponent implements OnInit {
     }
 
   }
-
-
-
-
 }
