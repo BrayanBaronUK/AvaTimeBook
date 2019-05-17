1b6d2b98cc0da581ced28e8757343cff49380518
DavidUK <36526045+c-esar@users.noreply.github.com>
2019-05-17 10:00:24 -0500

    Creacion y arreglo perfil otro

--- Codigo/src/app/perfil-otro/perfil-otro.component.ts
+++ Codigo/src/app/perfil-otro/perfil-otro.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input, Output, EventEmitter, ElementRef, ViewChild} from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ElementRef, ViewChild } from '@angular/core';
 import { UserService } from '../Core/user.service';
 import { Observable } from 'rxjs';
+import { ServicioLibroService } from '../Core/servicio-libro.service';
 import { AngularFireStorage } from 'angularfire2/storage';
 import { ServicioComentarioService } from '../Core/servicio-comentario.service';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
@@ -23,6 +24,7 @@ export class PerfilOtroComponent implements OnInit {
   urlImage: Observable<string>;
   public userFirebase: any;
   public userComentario = [];
+  public userLibro = [];
   public item = 1;
   public currentStatus = 1;
   private id: any;
@@ -33,9 +35,9 @@ export class PerfilOtroComponent implements OnInit {
   public idnuevo;
   text: String;
   images: any[];
-    // tslint:disable-next-line:no-inferrable-types
-    public display: boolean = false;
-    public usuarioEdit: any;
+  // tslint:disable-next-line:no-inferrable-types
+  public display: boolean = false;
+  public usuarioEdit: any;
   constructor(
     public UserServices: UserService,
     private storage: AngularFireStorage,
@@ -45,7 +47,8 @@ export class PerfilOtroComponent implements OnInit {
     public flashMensaje: FlashMessagesService,
     public router: Router,
     public fb: FormBuilder,
-    ) {
+    public UserLibro: ServicioLibroService
+  ) {
     // crea comentario;
     this.newcomentarioForm.setValue({
       ids: '',
@@ -53,7 +56,6 @@ export class PerfilOtroComponent implements OnInit {
     });
 
     this.text = `This will be converted to EmojiOne emojis! :thumbsup: ❤️`;
-    this.TraerInformacionUsuario();
   }
   // crea comentario
   public newcomentarioForm = new FormGroup({
@@ -63,17 +65,15 @@ export class PerfilOtroComponent implements OnInit {
 
   ngOnInit() {
     this.TraerComentario();
-    this.TraerInformacionUsuario();
-    this.fotos();
-    this.TraerInformacionUsuario2(this.idnuevo);
-  //  this.MostrarInformacion();
+    this.TraerInformacionUsuario(this.idLlego);
+    //  this.MostrarInformacion();
   }
 
 
 
   TraerComentario() {
-     // trae todos los comentarios
-     this.UserComentario.getComentario().subscribe((comentario) => {
+    // trae todos los comentarios
+    this.UserComentario.getComentarioOtroPerfil(this.idLlego).subscribe((comentario) => {
       this.userComentario = [];
       comentario.forEach((comentariodata: any) => {
         this.userComentario.push({
@@ -84,7 +84,7 @@ export class PerfilOtroComponent implements OnInit {
     });
   }
 
-  TraerInformacionUsuario() {
+  TraerInformacionUsuario(idLlego) {
     this.userFirebase = {
       nombre: '',
       apellido: '',
@@ -95,42 +95,23 @@ export class PerfilOtroComponent implements OnInit {
       nacionalidad: '',
       text: ''
     };
-    if(this.idLlego == null){
+    console.log("traer" + idLlego)
+    if (idLlego == null) {
       this.UserServices.getPerfil().valueChanges().subscribe((user) => {
-        console.log(this.userFirebase = user);
+        this.userFirebase = user;
       });
-      console.log("soy llego"+this.idLlego);
-    }else{
-          this.UserServices.getPerfilOtro(this.idLlego).valueChanges().subscribe((user) => {
-      console.log(this.userFirebase = user);
-    });
-    console.log("soy llego si llego"+this.idLlego);
+    } else {
+      this.UserServices.getPerfilOtro(this.idLlego).valueChanges().subscribe((user) => {
+        this.userFirebase = user;
+      });
     }
 
   }
 
-  TraerInformacionUsuario2(id) {
-    this.userFirebase = {
-      nombre: '',
-      apellido: '',
-      genero: '',
-      edad: '',
-      url: '',
-      celular: '',
-      nacionalidad: '',
-      text: ''
-    };
-    if(id == null){
-      this.UserServices.getPerfil().valueChanges().subscribe((user) => {
-        console.log(this.userFirebase = user);
-      });
-    }else{
-          this.UserServices.getPerfilOtro(id).valueChanges().subscribe((user) => {
-      console.log(this.userFirebase = user);
-    });
-    
-    }
-console.log("soy llego si llego"+id);
+  empezaCargarPerfil(id) {
+    this.idLlego = id;
+    console.log("empezar" + this.idLlego)
+    this.TraerInformacionUsuario(this.idLlego);
   }
 
 
@@ -152,17 +133,7 @@ console.log("soy llego si llego"+id);
   }
 
   // actualizar informacion usuario
-  onGuardarUsuarioUpdate() {
-    this.count = 1;
-    if (this.inputImageUser.nativeElement.value !== '' ) {
-      this.InformacionUsuarioProvicional.url = this.inputImageUser.nativeElement.value;
-      this.UserServices.updatePerfil(this.InformacionUsuarioProvicional);
-    } else {
-      this.UserServices.updatePerfil(this.InformacionUsuarioProvicional);
-    }
 
-    this.onCancelarUsuario();
-  }
 
 
 
@@ -173,53 +144,26 @@ console.log("soy llego si llego"+id);
     this.cerrar.emit();
   }
 
-  // envia datos del comentario
-  public newComentario(form) {
-    const data = {
-      text: form.text,
-      date: this.UserComentario.getTimeStamp()
-    };
-    this.UserComentario.createComentario(data).then(() => {
-      this.newcomentarioForm.setValue({
-        id: '',
-        text: ''
-      });
-    }, (error) => {
-      console.error(error);
-    });
-  }
   showDialog() {
     this.display = true;
   }
-
-  onUpload(e) {
-    const id = Math.random().toString(36).substring(2);
-    const file = e.target.files[0];
-    const filePath = `uploads/profile_${id}`;
-    const ref = this.storage.ref(filePath);
-    const task = this.storage.upload(filePath, file);
-    this.uploadPercent = task.percentageChanges();
-    task.snapshotChanges().pipe( finalize(() => this.urlImage = ref.getDownloadURL()))
-    .subscribe();
-  }
-
-  fotos() {
-    this.foto = {
-      url: ''
-    };
-    this.UserServices.getPerfil().valueChanges().subscribe((user) => {
-      console.log(this.foto = user);
+  TraerLibro() {
+    // trae todos los libros
+    this.UserLibro.getLibros(this.idLlego).subscribe((libros) => {
+      this.userLibro = [];
+      libros.map((librodata: any) => {
+        this.userLibro.push({
+          id: librodata.payload.doc.id,
+          nombre_libro: librodata.payload.doc.data().nombre_libro,
+          autor_libro: librodata.payload.doc.data().autor_libro,
+          categoria_libro: librodata.payload.doc.data().categoria_libro
+        });
+      });
     });
-    this.images = [];
-    // tslint:disable-next-line:no-debugger
-    debugger;
-    this.images.push({source: this.foto.url});
   }
+
+
 
   // FUNCIONES DE ELMININACION
-
-  EliminarComentario(id) {
-    this.UserComentario.deleteComentario(id);
-  }
 
 }
